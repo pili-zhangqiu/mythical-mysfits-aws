d9e6791df57b0c9b4e9b2dbf94b28eb7d12d3986
Pilar Zhang Qiu <p.zhangqiu@gmail.com>
2025-05-07 00:27:50 +0100

    fix(aws-s3): Fix for "upload failed: (...) The bucket does not allow ACLs"

--- frontend-deployment/deploy_frontend.sh
+++ frontend-deployment/deploy_frontend.sh
@@ -14,7 +14,7 @@ ng build --configuration production
 aws s3 mb s3://$S3_BUCKET_NAME --region $AWS_REGION || true
 aws s3 website s3://$S3_BUCKET_NAME --index index.html --error index.html
 aws s3 rm s3://$S3_BUCKET_NAME --recursive
-aws s3 cp $FRONTEND_BUILD_PATH s3://$S3_BUCKET_NAME --acl public-read --recursive
+aws s3 cp $FRONTEND_BUILD_PATH s3://$S3_BUCKET_NAME --recursive
 S3_BUCKET_LOCATION=$(aws s3api get-bucket-location --bucket $S3_BUCKET_NAME --query LocationConstraint --output text)
 if [ ! $S3_BUCKET_LOCATION = "None" ]; then
     echo "View your project here: http://$S3_BUCKET_NAME.s3-website.$S3_BUCKET_LOCATION.amazonaws.com"
